9c7cf49eccf2f19be272c96c941a2e5ac087af4b
Fabien Roger <efab.roger@gmail.com>
2023-03-22 09:54:09 -0700

    Final experiments fix

--- analyze_tokens_powers.py
+++ analyze_tokens_powers.py
@@ -1,10 +1,11 @@
 # %%
+from matplotlib.lines import lineStyles
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 plt.style.use("ggplot")
 # %%
-powers = pd.read_csv("powers.csv", index_col=0)
+powers = pd.read_csv("powers.csv")
 powers.head()
 # %%
 # print histograms of A and B powers
@@ -16,17 +17,59 @@ max_A_pow = powers["A_pow"].max()
 max_B_pow = powers["B_pow"].max()
 M = max(max_A_pow, max_B_pow)
 plt.hist([powers["A_pow"], powers["B_pow"]], bins=30, range=(m, M), label=["A", "B"], alpha=1) # type: ignore
+
+# ro_A_power = powers[powers["tok_str"] == " ro"]["A_pow"].item()
+# ro_B_power = powers[powers["tok_str"] == " ro"]["B_pow"].item()
+# ger_A_power = powers[powers["tok_str"] == "ger"]["A_pow"].item()
+# ger_B_power = powers[powers["tok_str"] == "ger"]["B_pow"].item()
+# plt.axvline(ro_A_power, color="red", label="' ro' A power", alpha=0.5, linestyle="--")
+# plt.axvline(ro_B_power, color="blue", label="' ro' B power", alpha=0.5, linestyle="--")
+# plt.axvline(ger_A_power, color="red", label="'ger' A power", alpha=0.5, linestyle="-.")
+# plt.axvline(ger_B_power, color="blue", label="'ger' B power", alpha=0.5, linestyle="-.")
+
 plt.legend()
 plt.xlabel("induction power")
 plt.ylabel("count")
 # %%
+from IPython.display import display
 # print top 10 A and B powers and bottom 10 A and B powers
 print("Top 10 A powers")
-print(powers.sort_values("A_pow", ascending=False).head(10))
-print("Top 10 B powers")
-print(powers.sort_values("B_pow", ascending=False).head(10))
-print("Bottom 10 A powers")
-print(powers.sort_values("A_pow", ascending=True).head(10))
-print("Bottom 10 B powers")
-print(powers.sort_values("B_pow", ascending=True).head(10))
+display(powers.sort_values("A_pow", ascending=False).head(10))
+display(powers.sort_values("B_pow", ascending=False).head(10))
+display(powers.sort_values("A_pow", ascending=True).head(10))
+display(powers.sort_values("B_pow", ascending=True).head(10))
+# print("Top 10 B powers")
+# print(powers.sort_values("B_pow", ascending=False).head(10))
+# print("Bottom 10 A powers")
+# print(powers.sort_values("A_pow", ascending=True).head(10))
+# print("Bottom 10 B powers")
+# print(powers.sort_values("B_pow", ascending=True).head(10))
 #%%
+# check for the " Fab" token
+print(powers[powers["tok_str"] == "ien"])
+# %%
+# %%
+from transformer_lens import HookedTransformer
+import torch
+
+# %%
+model_name = "attn-only-2l"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model: HookedTransformer = HookedTransformer.from_pretrained(model_name).to(device)  # type: ignore
+#%%
+for p in model.parameters():
+    p.requires_grad = False
+#%%
+def tokenize(text) -> torch.Tensor:
+    return model.to_tokens(text)
+
+
+def tokenize_no_bos(text) -> torch.Tensor:
+    return model.to_tokens(text, prepend_bos=False)
+
+
+def tokenize_strs(text) -> list[str]:
+    return model.to_str_tokens(text)
+
+print(tokenize_strs(" roger"))
+# %%
--- experiment.py
+++ experiment.py
@@ -1,7 +1,9 @@
 # %%
 from transformer_lens import HookedTransformer
 import torch
-
+import pandas as pd
+from matplotlib import pyplot as plt
+plt.style.use("ggplot")
 # %%
 model_name = "attn-only-2l"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -45,14 +47,18 @@ MIN_TOKEN = 4
 
 
 def random_induction_and_non_induction_seqs(
-    batch_size: int, seq_len: int, induction_pos: int, fixed_A: Optional[int] = None, fixed_B: Optional[int] = None
+    batch_size: int, seq_len: int, induction_pos: int, fixed_A: int | None | torch.Tensor = None, fixed_B: int | None | torch.Tensor = None
 ) -> tuple[torch.Tensor, torch.Tensor]:
     assert induction_pos >= 0 and induction_pos <= seq_len - 4
     seqs_with_induction = torch.randint(MIN_TOKEN, vocab_size, (batch_size, seq_len))
 
-    if fixed_A:
+    if fixed_A is not None:
+        if isinstance(fixed_A, torch.Tensor):
+            fixed_A = fixed_A[torch.randint(0, fixed_A.shape[0], (batch_size,))]
         seqs_with_induction[:, -2] = fixed_A
-    if fixed_B:
+    if fixed_B is not None:
+        if isinstance(fixed_B, torch.Tensor):
+            fixed_B = fixed_B[torch.randint(0, fixed_B.shape[0], (batch_size,))]
         seqs_with_induction[:, -1] = fixed_B
     seqs_with_induction[:, induction_pos : induction_pos + 2] = seqs_with_induction[:, -2:]
 
@@ -77,7 +83,7 @@ def find_nearest_neighbours(tokens: torch.Tensor, k: int = 10) -> torch.Tensor:
     normed_unembed = unembed_matrix / torch.norm(unembed_matrix, dim=0, keepdim=True)
     token_w = unembed_matrix[:, tokens]
     cosines = torch.einsum("ji,jl->li", normed_unembed, token_w)
-    _, inds = torch.topk(cosines, k=k, largest=True)
+    _, inds = torch.topk(cosines, k=k+1, largest=True)
     return inds[:, 1:].to(tokens.device)
 
 
@@ -118,10 +124,11 @@ from tqdm import trange
 gc.collect()
 
 fixed_strs = (" Fab", "ien")
+# fixed_strs = (" Fa", "ien")
 # fixed_strs = (" M", "arius")
 # fixed_strs = ("arius", " M")
 A, B = [int(tokenize_no_bos(s).item()) for s in fixed_strs]
-A, B = None, None
+# A, B = None, None
 # A = None
 # B = None
 
@@ -129,59 +136,82 @@ B_IDX = 0
 A_IDX = 1
 K = 3
 NN_IDXES = list(range(2, 2 + K))
-
+RANDOM_IDX = 2 + K
 
 def get_toks_of_interest(seqs_with_induction: torch.Tensor) -> torch.Tensor:
     A_toks = seqs_with_induction[:, -2]
     B_toks = seqs_with_induction[:, -1]
     B_nns = find_nearest_neighbours(B_toks, k=K)
-    return torch.stack([B_toks, A_toks, *B_nns.T], dim=-1)
+    rdm_toks = torch.randint(MIN_TOKEN, vocab_size, (seqs_with_induction.shape[0],))
+    return torch.stack([B_toks, A_toks, *B_nns.T, rdm_toks], dim=-1)
 
+scenarios = {
+    "fixed A=' Fab' and B='ien'": (A, B),
+    "fixed A=' Fab'": (A, None),
+    "fixed B='ien'": (None, B),
+    "no fixed A or B": (None, None),
+}
+
+diffs_per_scenarios = {}
+before_and_after_per_scenarios = {}
 
 length = 24
 pos = 10
-batch_size = 64
-N = 32
-all_diffs = []
-all_befores = []
-all_afters = []
-all_seqs_with = []
-all_seqs_without = []
-for _ in trange(30):
-    seqs_with, seqs_without = random_induction_and_non_induction_seqs(batch_size, length, pos, fixed_A=A, fixed_B=B)
-    diffs, before, after = measure_induction_power(seqs_with, seqs_without, get_toks_of_interest)
-    all_diffs.append(diffs)
-    all_seqs_with.append(seqs_with)
-    all_seqs_without.append(seqs_without)
-    all_befores.append(before)
-    all_afters.append(after)
-diffs_c = torch.cat(all_diffs, dim=0)
-seqs_with_c = torch.cat(all_seqs_with, dim=0)
-seqs_without_c = torch.cat(all_seqs_without, dim=0)
-befores_c = torch.cat(all_befores, dim=0)
-afters_c = torch.cat(all_afters, dim=0)
+batch_size = 256
+N = 16
+
+def measure(fixed_A=None, fixed_B=None):
+    all_diffs = []
+    all_befores = []
+    all_afters = []
+    for _ in trange(N):
+        seqs_with, seqs_without = random_induction_and_non_induction_seqs(batch_size, length, pos, fixed_A=fixed_A, fixed_B=fixed_B)
+        diffs, before, after = measure_induction_power(seqs_with, seqs_without, get_toks_of_interest)
+        all_diffs.append(diffs)
+        all_befores.append(before)
+        all_afters.append(after)
+    diffs_c = torch.cat(all_diffs, dim=0)
+    befores_c = torch.cat(all_befores, dim=0)
+    afters_c = torch.cat(all_afters, dim=0)
+    return diffs_c, befores_c, afters_c
+#%%
+
+for scenario_name, (fixed_A, fixed_B) in scenarios.items():
+    diffs_c, befores_c, afters_c = measure(fixed_A=fixed_A, fixed_B=fixed_B)
+    
+    diffs_per_scenarios[scenario_name] = diffs_c
+    before_and_after_per_scenarios[scenario_name] = (befores_c, afters_c)
 # #%%
 # print(diffs_c.mean(0))
 #%%
-from matplotlib import pyplot as plt
 
-plt.hist(diffs_c[:, 1].cpu().numpy(), bins=100, label="A", alpha=0.5)
-plt.hist(diffs_c[:, 0].cpu().numpy(), bins=100, label="B", alpha=0.5)
-plt.hist(diffs_c[:, 2].cpu().numpy(), bins=100, label="B nn", alpha=0.5)
-plt.title("log P on induction - log P on shuffled (last is still A)")
-plt.legend()
+for scenario_name, diffs_c in diffs_per_scenarios.items():
+    plt.hist(diffs_c[:, B_IDX].cpu().numpy(), bins=30, label=scenario_name, range=(-10, 20), histtype=u'step')
+plt.title("Histogram of induction strengths over 4096 sequences")
+plt.legend(loc="upper left")
 # %%
-# before v after heatmap (2D histogram)
-idx = NN_IDXES[0]
-# for idx, name in ((0, "B"),):
-for idx, name in ((0, "B"), (2, "B nn"), (1, "A")):
-    plt.hist2d(befores_c[:, idx].cpu().numpy(), afters_c[:, idx].cpu().numpy(), bins=100, range=((-20, 0), (-20, 0)))
-    plt.plot([10, -20], [10, -20], color="red")
-    plt.xlabel("induction")
-    plt.ylabel("shuffled")
-    plt.colorbar()
-    plt.title(name)
-    plt.show()
+m = -18
+M = 6
+fig, axs = plt.subplots(1, len(scenarios), figsize=(20, 5))
+for i, (scenario_name, (befores_c, afters_c)) in enumerate(before_and_after_per_scenarios.items()):
+    before_log_od = log_probs_to_log_odd_ratio(befores_c[:, B_IDX])
+    after_log_od = log_probs_to_log_odd_ratio(afters_c[:, B_IDX])
+    im = axs[i].hist2d(before_log_od.cpu().numpy(), after_log_od.cpu().numpy(), bins=100, range=((m, M), (m, M)), cmap="Blues")
+    
+    avg_point = (before_log_od.mean().cpu().item(), after_log_od.mean().cpu().item())
+    corresponding_point_on_diag = (avg_point[1], avg_point[1])
+    # draw double pointed arrow from avg point to corresponding point on diag and write "induction strength on it"
+    axs[i].annotate("", xy=avg_point, xytext=corresponding_point_on_diag, arrowprops=dict(arrowstyle="<->", color="black"))
+    x = (avg_point[0] + corresponding_point_on_diag[0]) / 2
+    y = (avg_point[1] + corresponding_point_on_diag[1]) / 2
+    axs[i].text(x, y, "avg induction\nstrength", color="black", ha="center", va="center")
+    axs[i].plot([M, m], [M, m], color="black")
+    axs[i].set_xlabel("log odd-ratio on induction")
+    axs[i].set_ylabel("log odd-ratio on shuffled")
+    axs[i].set_title(scenario_name)
+    axs[i].set_aspect("equal")
+fig.suptitle("2D histogram of log odd-ratios before and after shuffling the sequences\n")
+plt.tight_layout()
 # %%
 import numpy as np
 
@@ -219,17 +249,59 @@ def token_powers(tok) -> tuple[float, float, float, float]:
 
 all_powers = []
 # %%
-for i in trange(MIN_TOKEN, MIN_TOKEN + 10000):
-    tok = i
-    A_pow, B_pow, A_pow_std, B_pow_std = token_powers(tok)
-    tok_str = model.tokenizer.decode([tok])  # type: ignore
-    all_powers.append((tok, tok_str, A_pow, B_pow, A_pow_std, B_pow_std))
-#%%
-copy = all_powers.copy()
-# %% save to df
-import pandas as pd
+recompute = False
 
-for i, (tok, tok_str, A_pow, B_pow, A_pow_std, B_pow_std) in enumerate(all_powers):
-    tok_str_escaped = repr(tok_str)[1:-1]
-df = pd.DataFrame(all_powers, columns=["tok", "tok_str", "A_pow", "B_pow", "A_pow_std", "B_pow_std"])
-df.to_csv("powers.csv", escapechar="\\", index=False)
+if recompute:
+    for i in trange(MIN_TOKEN, MIN_TOKEN + 10000):
+        tok = i
+        A_pow, B_pow, A_pow_std, B_pow_std = token_powers(tok)
+        tok_str = model.tokenizer.decode([tok])  # type: ignore
+        all_powers.append((tok, tok_str, A_pow, B_pow, A_pow_std, B_pow_std))
+
+    for i, (tok, tok_str, A_pow, B_pow, A_pow_std, B_pow_std) in enumerate(all_powers):
+        tok_str_escaped = repr(tok_str)[1:-1].replace("\n", "\\n").replace("\t", "\\t").replace("\r", "\\r").replace("\f", "\\f")
+        all_powers[i] = (tok, tok_str_escaped, A_pow, B_pow, A_pow_std, B_pow_std)
+    df = pd.DataFrame(all_powers, columns=["tok", "tok_str", "A_pow", "B_pow", "A_pow_std", "B_pow_std"])
+    df.to_csv("powers.csv", escapechar="\\", index=False)
+#%%
+# df = pd.read_csv("powers.csv", escapechar="\\")
+# proportion = 0.1
+# top_A_tokens = torch.tensor(df.sort_values("A_pow", ascending=False).head(int(len(df) * proportion))["tok"].astype(int).values, dtype=torch.long)
+K = 20
+NN_IDXES = list(range(2, 2 + K))
+RANDOM_IDX = 2 + K
+diffs, *_ = measure()
+plt.hist(diffs[:, B_IDX].cpu().numpy(), bins=30, label="B", histtype=u'step')
+for i in [0, 4, 14]:
+    n_neighb = NN_IDXES[i]
+    name = f"{i+1}th closest token to B"
+    plt.hist(diffs[:, n_neighb].cpu().numpy(), bins=30, label=name, histtype=u'step')
+plt.hist(diffs[:, RANDOM_IDX].cpu().numpy(), bins=30, label="Random tokens", histtype=u'step', color="black")
+plt.xlabel("induction strength")
+plt.ylabel("count")
+plt.title("Induction strength of for B and tokens near B")
+plt.legend()
+# for i in [0,1, 5, 15]:
+#     idx = i + 1 if i != 0 else 0
+#     label = f"{i}th closest token to B" if i != 0 else "B"
+#     plt.hist(diffs[:, idx].cpu().numpy(), bins=30, alpha=0.5, label=label)
+# plt.legend()
+# #%%
+# bot_A_tokens = torch.tensor(df.sort_values("A_pow", ascending=True).head(int(len(df) * proportion)).index.astype(int).values, dtype=torch.long)
+# K = 20
+# diffs, *_ = measure(bot_A_tokens, None)
+
+# for i in [0,1, 5, 15]:
+#     idx = i + 1 if i != 0 else 0
+#     label = f"{i}th closest token" if i != 0 else "B"
+#     plt.hist(diffs[:, idx].cpu().numpy(), bins=30, alpha=0.5, label=label)
+# plt.legend()
+# # %%
+# # 'a' defined earlier as pandas index, find place where it is not finite
+# for i, row in df.iterrows():
+#     if not np.isfinite(row["A_pow"]):
+#         print(i,row)
+#         break
+# # %%
+
+# %%
